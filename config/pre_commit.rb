2fe4452a19f18368233960fd670ccf254ba4c0ba
kandaharu <yuugengaisha@gmail.com>
2015-05-02 17:36:17 +0900

    add pre-commit gem settings

---
+++ config/pre_commit.rb
@@ -0,0 +1,45 @@
+#! /usr/bin/env ruby
+# coding: utf-8
+
+# コンソール上に表示する文字に色を付ける
+require 'colorize'
+
+# colorize の gem が入ってないときにとりあえず動くようにする。
+unless String.instance_methods.include?(:colorize)
+  class String
+    def colorize(*_args)
+      self
+    end
+  end
+end
+
+# == コミット対象に config フォルダ下のファイルが含まれているチェック
+# 含まれていたら警告するメッセージを出力
+# === 戻り値
+# +true+:: コミットOK
+# +false+:: コミットNG
+def warn_including_config_file(files)
+  config_files = files.select { |file| file.match %r{config/} }
+
+  return true if config_files.empty?
+
+  warn_message = <<-"EOS"
+  [WARN] コミット対象のファイルに config フォルダ下のファイルが含まれています。
+    #{config_files.join(', ')}
+  EOS
+
+  puts warn_message.colorize(:yellow)
+  false
+end
+
+# == コミット可能かチェック
+# === 引数
+# +files+:: コミット対象のファイル
+# === 戻り値
+# +true+:: コミットOK
+# +false+:: コミットNG
+def commit?(files)
+  warn_including_config_file(files)
+end
+
+exit commit?(ARGV) ? 0 : 1
